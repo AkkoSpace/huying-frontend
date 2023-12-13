9ebb9610a214dd87bd4c7da8fa7c3e2d491750af
Akko <akkoyan@qq.com>
2023-12-14 00:06:28 +0800

    :construction: feat: 订单管理页面布局

--- src/views/transaction/order/index.vue
+++ src/views/transaction/order/index.vue
@@ -1,12 +1,172 @@
 <template>
   <div h-full w-full>
-    <div class="h-[200px]">
-      <a-card h-full> 111</a-card>
+    <div class="h-[180px]" flex flex-row pt-4 px-3>
+      <div class="w-3/4">
+        <a-card h-full> 333</a-card>
+      </div>
+      <div class="w-1/4" pl-2>
+        <a-card h-full> 4444</a-card>
+      </div>
     </div>
-    <div class="h-[calc(100vh_-_300px)]">
-      <a-card h-full> 222</a-card>
+    <div class="h-[calc(100vh_-_280px)] card-custom" pt-4 px-3>
+      <div flex flex-row h-full>
+        <div class="w-3/4">
+          <a-card h-full overflow-auto>
+            <a-table
+              :bordered="false"
+              :columns="columns"
+              :data="data"
+              :pagination="false"
+            />
+          </a-card>
+        </div>
+        <div class="w-1/4" pl-2>
+          <div class="h-1/4">
+            <a-card h-full> 1/4</a-card>
+          </div>
+          <div class="h-3/4" pt-2>
+            <a-card h-full> 3/4</a-card>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script lang="ts" setup>
+  import { reactive } from 'vue';
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Salary',
+      dataIndex: 'salary',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+    },
+  ];
+  const data = reactive([
+    {
+      key: '1',
+      name: 'Jane Doe',
+      salary: 23000,
+      address: '32 Park Road, London',
+      email: 'jane.doe@example.com',
+    },
+    {
+      key: '2',
+      name: 'Alisa Ross',
+      salary: 25000,
+      address: '35 Park Road, London',
+      email: 'alisa.ross@example.com',
+    },
+    {
+      key: '3',
+      name: 'Kevin Sandra',
+      salary: 22000,
+      address: '31 Park Road, London',
+      email: 'kevin.sandra@example.com',
+    },
+    {
+      key: '4',
+      name: 'Ed Hellen',
+      salary: 17000,
+      address: '42 Park Road, London',
+      email: 'ed.hellen@example.com',
+    },
+    {
+      key: '5',
+      name: 'William Smith',
+      salary: 27000,
+      address: '62 Park Road, London',
+      email: 'william.smith@example.com',
+    },
+    {
+      key: '11',
+      name: 'Jane Doe',
+      salary: 23000,
+      address: '32 Park Road, London',
+      email: 'jane.doe@example.com',
+    },
+    {
+      key: '12',
+      name: 'Alisa Ross',
+      salary: 25000,
+      address: '35 Park Road, London',
+      email: 'alisa.ross@example.com',
+    },
+    {
+      key: '13',
+      name: 'Kevin Sandra',
+      salary: 22000,
+      address: '31 Park Road, London',
+      email: 'kevin.sandra@example.com',
+    },
+    {
+      key: '14',
+      name: 'Ed Hellen',
+      salary: 17000,
+      address: '42 Park Road, London',
+      email: 'ed.hellen@example.com',
+    },
+    {
+      key: '15',
+      name: 'William Smith',
+      salary: 27000,
+      address: '62 Park Road, London',
+      email: 'william.smith@example.com',
+    },
+    {
+      key: '21',
+      name: 'Jane Doe',
+      salary: 23000,
+      address: '32 Park Road, London',
+      email: 'jane.doe@example.com',
+    },
+    {
+      key: '22',
+      name: 'Alisa Ross',
+      salary: 25000,
+      address: '35 Park Road, London',
+      email: 'alisa.ross@example.com',
+    },
+    {
+      key: '23',
+      name: 'Kevin Sandra',
+      salary: 22000,
+      address: '31 Park Road, London',
+      email: 'kevin.sandra@example.com',
+    },
+    {
+      key: '24',
+      name: 'Ed Hellen',
+      salary: 17000,
+      address: '42 Park Road, London',
+      email: 'ed.hellen@example.com',
+    },
+    {
+      key: '25',
+      name: 'William Smith',
+      salary: 27000,
+      address: '62 Park Road, London',
+      email: 'william.smith@example.com',
+    },
+  ]);
+</script>
+
+<style scoped>
+  .card-custom {
+    :deep(.arco-card-body) {
+      @apply h-full;
+    }
+  }
+</style>
