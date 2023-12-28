749349e2dbca93e46f475528a9eeed6ff6f5e143
yantingwei <akkoyan@qq.com>
2023-12-28 15:02:22 +0800

    :fire: refactor: 重构产品页面

---
+++ src/views/products/list/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div> ProductsList </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<script lang="ts">
+  export default {
+    name: 'ProductsList', // If you want the include property of keep-alive to take effect, you must name the component
+  };
+</script>
